85440868abb06aa8e3ccd818ee8e5fdcdfa89aa4
Jose Cota <89209438+Yuzemait@users.noreply.github.com>
2024-04-09 08:37:43 -0600

    Add files via upload

---
+++ features/step_defs/report_user.js
@@ -0,0 +1,43 @@
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('Estoy en la pagina de un usuario de PalPet', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+Given('Estoy en el formulario de reporte de usuario', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+Given('Tengo una cuenta de MiApoyoIteso', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('Selecciono el boton Reportar Perfil', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+  When('Agrego detalles del reporte', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+  
+  When('Envio el formulario completo', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  
+  Then('Veo un formulario donde adjunto la informacion y evidencia del reporte', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+  Then('Me aparecen posibles razones del reporte', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+  Then('Recibo una confirmacion del reporte enviado', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
