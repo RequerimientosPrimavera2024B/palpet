dc0b86b11b05a5a780fc21226e1447766e435670
RoyalMoon96 <diegoo_doo@hotmail.com>
2024-05-04 14:09:05 -0600

    Poder_pasar_fotos_y_videos

---
+++ features/step_defs/stepdefs_Poder_pasar_fotos_y_videos.js
@@ -0,0 +1,116 @@
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('el usuario esta verificado', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+ 
+When('el usuario accede a la opción de subir foto\\/video', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+ 
+Then('el usuario puede acceder a la opción de subir foto\\/video', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+ 
+Given('el usuario no esta verificado', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+  
+
+Then('el usuario no puede acceder a la opción de subir foto\\/video', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+ 
+Given('el usuario permitió el acceso a la galería', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+  
+  
+Given('el usuario no permitió el acceso a la galería', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+ 
+Then('se informa al usuario y le pregunta si desea tomar una foto\\/video', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+ 
+Given('En el que el usuario edita la imagen \\/ video', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+ 
+When('el usuario sube una foto\\/video', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+ 
+Then('se informa en el chat que la foto\\/video fue alterada', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+   
+
+Then('se informa en el chat que la foto\\/video no fue alterada por medio de la app', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+ 
+Given('existia una foto\\/video en el chat', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+ 
+When('la foto\\/video es eliminada del chat', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+ 
+Then('se informa en el chat que la foto\\/video fue eliminada', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+  
+ 
+When('la foto\\/video permanece en el chat', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+}); 
+
+ 
+ 
+Given('existie una foto\\/video en el chat', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+ 
+When('la foto\\/video es reportada o detectada que no cumple las normas de la app', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+ 
+Then('se elimina y se le informa en el chat que la foto\\/video fue eliminada', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+  
+ 
+ When('la foto \\/ video cumple las normas.', function () {
+   // Write code here that turns the phrase above into concrete actions
+   return 'pending';
+ });
+  
