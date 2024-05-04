6a83ee0cfd043edb42e5c196c8989b35e1a149a8
RoyalMoon96 <diegoo_doo@hotmail.com>
2024-05-04 15:27:44 -0600

    Donaciones

---
+++ features/step_defs/stepdefs_Donaciones.js
@@ -0,0 +1,206 @@
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+When('el usuario intenta acceder  a la sección de {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+ 
+  Then('el usuario puede acceder a la sección de {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+ 
+  When('el usuario intenta acceder a la sección de {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+ 
+  Then('se le pide al usuario verificar su cuenta', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+ 
+  Given('el usuario intenta Donar', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+ 
+  When('el usuario cancela la opción', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  }); 
+
+  Then('se cancela la doncacion y se regresa al menu de donaciones', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+  
+ 
+  When('el usuario selecciona el beneficiario', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+ 
+  Then('continua con la donacion \\(pago)', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  
+ 
+  When('el usuario ingresa un monto no valido', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+ 
+  Then('se le pide al usuario cambiar el montoa uno valido', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });  
+
+ 
+ 
+  When('el usuario ingresa un monto valido', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+ 
+  Then('se continua con el pago', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+ 
+  Given('el usuario Dono', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+  
+ 
+  Then('se notifica del exito de la donacion', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+ 
+  Given('el usuario no dono', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+ 
+  When('el usuario finaliza la donacion', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  }); 
+
+  Then('se notifica de la cancelacion de la donacion', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+ 
+  Given('el usuario Dono y el beneficiario reciobio el monto', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+ 
+  When('Se finaliza la donacion', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+ 
+  Then('se notifica del exito de la transacción', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+ 
+   Given('beneficiario no es valido \\(no esta verificado)', function () {
+     // Write code here that turns the phrase above into concrete actions
+     return 'pending';
+   });
+  
+   When('el usuario selecciona el beneficiario no es valido', function () {
+     // Write code here that turns the phrase above into concrete actions
+     return 'pending';
+   });
+  
+   Then('se le informa al usuario que el beneficiario no es valido', function () {
+     // Write code here that turns the phrase above into concrete actions
+     return 'pending';
+   });
+ 
+ 
+   Given('beneficiario es valido \\(esta verificado)', function () {
+     // Write code here that turns the phrase above into concrete actions
+     return 'pending';
+   });
+  
+  
+  
+   Then('se inicia el proceso de donacion', function () {
+     // Write code here that turns the phrase above into concrete actions
+     return 'pending';
+   });
+ 
+ 
+   Given('En el que el beneficiario no cuenta con donaciones', function () {
+     // Write code here that turns the phrase above into concrete actions
+     return 'pending';
+   });
+  
+   When('el usuario intenta ver el historial de donaciones de el beneficiario', function () {
+     // Write code here that turns the phrase above into concrete actions
+     return 'pending';
+   });
+  
+   Then('se indica que aun no ha recibido donaciones', function () {
+     // Write code here that turns the phrase above into concrete actions
+     return 'pending';
+   });
+ 
+ 
+   Given('En el que el beneficiario no ha usado sus donaciones', function () {
+     // Write code here that turns the phrase above into concrete actions
+     return 'pending';
+   });
+  
+   When('el usuario intenta ver el historial de fondos de el beneficiario', function () {
+     // Write code here that turns the phrase above into concrete actions
+     return 'pending';
+   });
+  
+   Then('se indica que aun no ha usado sus fondos', function () {
+     // Write code here that turns the phrase above into concrete actions
+     return 'pending';
+   });
+ 
+  
+  
+   When('PalPet publica el informe periódico sobre el impacto de las donaciones', function () {
+     // Write code here that turns the phrase above into concrete actions
+     return 'pending';
+   });
+  
+   Then('se indica que la organización no se generado ningún impacto', function () {
+     // Write code here that turns the phrase above into concrete actions
+     return 'pending';
+   });
+ 
+ 
+   Given('un usuario hace una donacion', function () {
+     // Write code here that turns the phrase above into concrete actions
+     return 'pending';
+   });
+  
+   When('el usuario resalta sobre otras donaciones', function () {
+     // Write code here that turns the phrase above into concrete actions
+     return 'pending';
+   });
+ 
+ 
+   Then('el usuario es destacado entre los donantes y se le da una recompenza \\(tiempo de palpetPlus)', function () {
+     // Write code here that turns the phrase above into concrete actions
+     return 'pending';
+   });
