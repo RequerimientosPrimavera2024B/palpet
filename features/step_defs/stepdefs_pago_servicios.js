458d902e89ffad9362403e5620067fef54870f53
RoyalMoon96 <diegoo_doo@hotmail.com>
2024-05-04 13:43:46 -0600

    Pago de servicios

---
+++ features/step_defs/stepdefs_pago_servicios.js
@@ -0,0 +1,120 @@
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+ 
+         Given('el usuario esta logeado', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+        
+         When('el usuario intenta accede a el apartado pagar un servicio', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+        
+         Then('se consultan los servicios pendientes por pagar', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+        
+         Given('el usuario no esta logeado', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+        
+         When('el usuario intenta acceder a el apartado pagar un servicio', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+        
+         Then('se pide al usuario que este logeado', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+  
+         When('el que el usuario no ha pagado un servicio solicitado', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+        
+         Then('se le pide al usuario que pague el servicio o lo cancele', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+  
+         When('el método de pago no ha sido dado de alta', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+        
+         Then('se le pide al usuario que de dealta un método', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+         
+        
+         When('el método de pago es correcto', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+        
+         Then('se efectua el cobro', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+         
+        
+         When('ocurrio un error', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         }); 
+
+         Then('se le informa al usuario y se regresa al menu de servicios pendientes', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+  
+        
+         When('los datos dados son erroneos', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+        
+         Then('se le informa al usuario y se le niega continuar \\(hasta que sean correctos)', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+  
+        
+         When('los datos dados son correctos', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+        
+         Then('continua el pago', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+ 
+         Given('el usuario pudo pagar', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+        
+         When('el usuario finalizo el pago', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+    
+         
+          When('el usuario esta intentando pagar', function () {
+            // Write code here that turns the phrase above into concrete actions
+            return 'pending';
+          });
+ 
